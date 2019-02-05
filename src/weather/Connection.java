7ef47a5ab76d6665dc75309e816cc2712e469a7f
Detmer Struiksma <detmer56@gmail.com>
2019-02-05 13:03:24 +0100

    Finished client connection

--- src/weather/Connection.java
+++ src/weather/Connection.java
@@ -1,36 +1,56 @@
 package weather;
 
-import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class Connection {
-	String hostName;
-	int portNumber;
-	
-	public Connection() {
-		String hostName = "host";
-		int portNumber = 7789;
-		
-	}
-	
-	public void connect() {
-		try {
-			    Socket echoSocket = new Socket(hostName, portNumber);
-			    PrintWriter out =
-			        new PrintWriter(echoSocket.getOutputStream(), true);
-			    BufferedReader in =
-			        new BufferedReader(
-			            new InputStreamReader(echoSocket.getInputStream()));
-			    BufferedReader stdIn =
-			        new BufferedReader(
-			            new InputStreamReader(System.in));
-		    }  catch(IOException e) {
-		    	e.printStackTrace();
-				
-			}
-	}
+
+  public final static int SOCKET_PORT = 13267;      // you may change this
+  public final static String SERVER = "127.0.0.1";  // localhost
+  public final static String
+       FILE_TO_RECEIVED = "c:/file.xml";  // you may change this, I give a
+                                                            // different name because i don't want to
+                                                            // overwrite the one used by server...
+
+  public final static int FILE_SIZE = Integer.MAX_VALUE;
+
+  public static void main (String [] args ) throws IOException {
+    int bytesRead;
+    int current = 0;
+    FileOutputStream fos = null;
+    BufferedOutputStream bos = null;
+    Socket sock = null;
+    try {
+      sock = new Socket(SERVER, SOCKET_PORT);
+      System.out.println("Connecting...");
+
+      // receive file
+      byte [] mybytearray  = new byte [FILE_SIZE];
+      InputStream is = sock.getInputStream();
+      fos = new FileOutputStream(FILE_TO_RECEIVED);
+      bos = new BufferedOutputStream(fos);
+      bytesRead = is.read(mybytearray,0,mybytearray.length);
+      current = bytesRead;
+
+      do {
+         bytesRead =
+            is.read(mybytearray, current, (mybytearray.length-current));
+         if(bytesRead >= 0) current += bytesRead;
+      } while(bytesRead > -1);
+
+      bos.write(mybytearray, 0 , current);
+      bos.flush();
+      System.out.println("File " + FILE_TO_RECEIVED
+          + " downloaded (" + current + " bytes read)");
+    }
+    finally {
+      if (fos != null) fos.close();
+      if (bos != null) bos.close();
+      if (sock != null) sock.close();
+    }
+  }
 
 }
